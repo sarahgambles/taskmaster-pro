28aa82d1bd6c4e7bcf5a75fdfdb5b2bd6f564421
Sarah Gambles <sarahgambles14@gmail.com>
2020-08-26 23:08:18 -0600

    updated the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,20 +94,51 @@ $(".list-group").on("blur", "textarea", function() {
 $(".list-group").on("click", "span", function() {
   // get current text
   var date = $(this)
-  .text()
-  .trim();
+    .text()
+    .trim();
 
   // create new input element
   var dateInput = $("<input>")
-  .attr("type", "text")
-  .addClass("form-control")
-  .val(date);
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
 
   // swap out elements
   $(this).replaceWith(dateInput);
 
   //automatically focus on new element
   dateInput.trigger("focus");
+});
+
+// value of date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+// get the parent ul's id attribute
+var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+// get the task's position in the list of other li elements
+var index = $(this)
+  .closes(".list-group-item")
+  .index();
+
+// update task in array and re-save to localstorage
+tasks[status][index].date = date;
+saveTasks();
+
+// recreate span element with bootstrap classes
+var taskSpan = $("<span>")
+  .addClass("badge badge-primary badge-pill")
+  .text(date);
+
+// replace input with span element
+$(this).replaceWith(taskSpan);
 });
 
 // modal was triggered
